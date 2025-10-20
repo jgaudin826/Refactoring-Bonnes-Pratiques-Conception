d4b44f5ef43504b1628caef324a560f9fbedc65b
jgaudin826 <jgaudin826@gmail.com>
2025-10-20 14:52:58 +0200

    added connect and disconnect forms in html file and main.go

--- services/authentification.go
+++ services/authentification.go
@@ -2,14 +2,12 @@ package services
 
 import "net/http"
 
-func Signup(w http.ResponseWriter, r *http.Request) {
+func Connect(w http.ResponseWriter, r *http.Request) {
 	email := r.FormValue("email")
 	SetCookie(email, w)
-}
-
-func Login(w http.ResponseWriter, r *http.Request) {
-	email := r.FormValue("email")
-	SetCookie(email, w)
+	if api.getUsers("data.json") {email} {
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+	}
 }
 
 func Disconnect(w http.ResponseWriter, r *http.Request) {
