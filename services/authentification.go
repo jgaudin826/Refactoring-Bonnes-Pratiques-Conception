b842b7026426b80c979667b886bcae46a6aff4dc
jgaudin826 <jgaudin826@gmail.com>
2025-10-20 14:25:40 +0200

    added authentification and cookies

---
+++ services/authentification.go
@@ -0,0 +1,17 @@
+package services
+
+import "net/http"
+
+func Signup(w http.ResponseWriter, r *http.Request) {
+	email := r.FormValue("email")
+	SetCookie(email, w)
+}
+
+func Login(w http.ResponseWriter, r *http.Request) {
+	email := r.FormValue("email")
+	SetCookie(email, w)
+}
+
+func Disconnect(w http.ResponseWriter, r *http.Request) {
+	DeleteCookie(w)
+}
